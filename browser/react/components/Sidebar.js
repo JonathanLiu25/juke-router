edd239e729d8850bb3bf1f5ab113215ee585ca92
JonathanLiu25 <JonathanLiu25@gmail.com>
2017-03-28 18:34:09 -0400

    completed Link

--- browser/react/components/Sidebar.js
+++ browser/react/components/Sidebar.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router'
 
-const Sidebar = (props) => {
-
-  const deselectAlbum = props.deselectAlbum;
+const Sidebar = () => {
 
   return (
     <sidebar>
       <img src="juke.svg" className="logo" />
       <section>
-        <h4 className="menu-item active">
-          <a href="#" onClick={deselectAlbum}>ALBUMS</a>
-        </h4>
+        <ul className="sidebar-menu">
+          <li>
+            <h4 className="menu-item active">
+              <Link to='/albums'>ALBUMS</Link>
+            </h4>
+          </li>
+          <li>
+            <h4 className="menu-item">
+              <Link to='/artists'>ARTISTS</Link>
+            </h4>
+          </li>
+        </ul>
       </section>
     </sidebar>
   );
